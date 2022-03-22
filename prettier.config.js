835e5f84e9b8033d6ee4a2ad1e588bc1c7a7e159
Kevin Wang <kwangsan@gmail.com>
2022-03-21 22:40:26 -0400

    prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,7 @@
-export const trailingComma = "es5";
-export const tabWidth = 2;
-export const semi = false;
-export const singleQuote = true;
+module.exports = {
+	trailingComma: "es5",
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	printWidth: 100
+}
